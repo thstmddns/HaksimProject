3ff16e57910324223feecb0a33207f2cc44a01fc
Sooin <tkrhkthsu503@naver.com>
2023-07-26 15:14:39 +0900

    feat:최종?

--- TeamProject_Team_3/TeamProject/src/main/java/kr/or/smhrd/dto/ReportDTO.java
+++ TeamProject_Team_3/TeamProject/src/main/java/kr/or/smhrd/dto/ReportDTO.java
@@ -15,13 +15,9 @@ public class ReportDTO {
 	private String report_content;
 	private String report_wdate;
 	private String report_url;
-<<<<<<< HEAD
 	private String reportReply_url;
 	private String board;
 	private int grad_num;
 	private int grad_review_num;
-=======
-	private String report_board;
-	private String report_board_num;
->>>>>>> 0474079d0e8d3ca7c2461289f2a277041d0dfeee
+
 }
