5884b57d861e8ab1e78c1600f0e59f06ae2b1f44
Sooin <tkrhkthsu503@naver.com>
2023-07-28 11:41:22 +0900

    프론트 최종!!!!!!!

--- TeamProject_Team_3/TeamProject/src/main/java/kr/or/smhrd/dao/ReportDAO.java
+++ TeamProject_Team_3/TeamProject/src/main/java/kr/or/smhrd/dao/ReportDAO.java
@@ -6,19 +6,19 @@ import kr.or.smhrd.dto.NoticeDTO;
 import kr.or.smhrd.dto.ReportDTO;
 
 public interface ReportDAO {
-	// �Ű� ���� ����Ʈ
+	
 	public List<ReportDTO> getReportList();
 	
-	// �Ű� �Խñ� �� ��ȸ
+	
 	public ReportDTO ReportBoardView(int no);
 	
-	// �Ű� �Խñ� ����
+	
 	public int ReportBoardDel(int no, String id);
 	
-	// �Ű� ��� �� ��ȸ
+	
 	public ReportDTO ReportReplyView();
 	
-	// �Ű� ��� ���� 
+	
 	public int ReportReplyDel(int no, String id);
 
 	public int ReportDel(int no);
